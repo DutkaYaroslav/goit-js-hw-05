613a094c676eec2700313b0aa8d1c3ad74749102
unknown <yaroslavdutka@gmail.com>
2020-03-28 16:38:41 +0200

    5hw

---
+++ hw5/js/test.js
@@ -0,0 +1,18 @@
+// const TodoEditor = function (todos = []) {
+//     this.todos = todos;
+// }
+
+
+
+// TodoEditor.prototype.saveTodo = function (todo) {
+//     this.todos.push(todo);
+// }
+
+// const todoEditor = new TodoEditor();
+
+
+// todoEditor.saveTodo({
+//     text: 'new todo'
+// });
+
+// console.log(todoEditor)
